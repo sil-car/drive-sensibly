6f09288d591794ee095c32cc63d34c578500d010
Nate Marti <nate_marti@sil.org>
2021-04-30 11:53:49 +0100

    improve ownership tests

--- drive-sensibly.py
+++ drive-sensibly.py
@@ -57,6 +57,7 @@ def get_creds(client_secrets, scopes):
     auth_url, _ = flow.authorization_url(prompt='consent')
     print(f'\nUse this link for authorization: {auth_url}')
     code = input('\nAuth. code: ').strip()
+    print()
     flow.fetch_token(code=code)
     creds = flow.credentials
     return creds
@@ -68,7 +69,10 @@ def get_drive_service(credentials):
 def get_drive_search_results(service, query, page_token=None):
     """Get results of search query on specified account."""
     # Get all useful fields at one time to minimize network traffic.
-    fields = 'nextPageToken, files(id, name, mimeType, modifiedTime, ownedByMe, sharedWithMeTime, parents, permissions)'
+    fields = '\
+        nextPageToken, files(id, name, mimeType, modifiedTime, ownedByMe, \
+        sharedWithMeTime, owners, parents, permissions)\
+    '
     results = []
     while True:
         try:
@@ -93,7 +97,7 @@ def get_item(service, name_string, type='folder'):
     """Search for "folder_string" among Drive folders and folder IDs."""
     name_escaped = name_string.replace("'", "\\'") # doesn't work
     name_escaped = name_string
-    q = f"name = '{name_escaped}'"
+    q = f"name = '{name_escaped}' and not trashed"
     if type == 'folder':
         q = f"name = '{name_escaped}' and \
             mimeType = 'application/vnd.google-apps.folder' and \
@@ -102,8 +106,8 @@ def get_item(service, name_string, type='folder'):
     if len(results) > 1:
         print(f"{len(results)} results found. Please specify which item to handle:")
         for i, item in enumerate(results):
-            item_tree = get_tree(service, item)
-            print(f"   {i+1}. {item_tree}")
+            parents_string = get_parents_string(service, item)
+            print(f"   {i+1}. {parents_string}")
         item_num = int(input("\nEnter number: ").replace('.', '').strip())
         print()
         item_id = results[item_num-1]['id']
@@ -123,10 +127,16 @@ def get_children(service, folder_id):
     return children
 
 def change_item_owner(service, item, new_owner):
-    current_user_owns = item.get('ownedByMe', None)
-    if not current_user_owns:
-        # Can't transfer ownership of not owner.
+    # Check current ownership.
+    owners = item.get('owners', None)
+    for o in owners:
+        if o['emailAddress'] == new_owner:
+            # Permissions are already correct.
+            return True
+    if not item.get('ownedByMe', None):
+        # Can't transfer ownership if not current owner.
         return False
+
     # Transfer ownership.
     permissions = item.get('permissions', None)
     for permission in permissions:
@@ -147,7 +157,7 @@ def change_item_owner(service, item, new_owner):
                 print(f"Error: {e}")
                 return False
 
-def get_tree(service, folder):
+def get_parents_string(service, folder):
     parents = list_parents_recursively(service, folder)
     tree = [p['name'] for p in parents]
     tree.insert(0, folder['name'])
@@ -156,27 +166,17 @@ def get_tree(service, folder):
     return tree_string
 
 def change_owner_recursively(service, item, new_owner, parents=list()):
+    # Handle item.
     result = change_item_owner(service, item, new_owner)
-    x = '\u2717'
-    if result:
-        x = '\u2713'
+    x = '\u2713' if result else '\u2717'
     print(f"{x} {' > '.join(parents)}")
-    item_id = item.get('id')
-    children = children = get_children(service, item_id)
-    for child in children:
-        item = service.files().get(
-            fileId=child['id'],
-            fields='id, name, mimeType, modifiedTime, ownedByMe, sharedWithMeTime, parents, permissions',
-        ).execute()
-        if item['mimeType'] == 'application/vnd.google-apps.folder':
-            parents.append(item['name'])
-            change_owner_recursively(service, item, new_owner, parents)
-        else:
-            result = change_item_owner(service, item, new_owner)
-            x = '\u2717'
-            if result:
-                x = '\u2713'
-            print(f"{x} {' > '.join([*parents, item['name']])}")
+
+    # Handle item's children.
+    if item['mimeType'] == 'application/vnd.google-apps.folder':
+        children = get_children(service, item['id'])
+        for child in children:
+            new_parents = [*parents, child['name']]
+            change_owner_recursively(service, child, new_owner, new_parents)
 
 def list_parents_recursively(service, folder, parents=list()):
     parents1_ids = folder.get('parents', [])
