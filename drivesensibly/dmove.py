a8b075a2dbd86fd3243b3b3d2e501a5c648e514e
Nate Marti <nate_marti@sil.org>
2021-05-05 10:01:58 +0100

    error catching

--- drivesensibly/dmove.py
+++ drivesensibly/dmove.py
@@ -24,18 +24,24 @@ def remove_drive_item(service, item, type='delete'):
     try:
         if type == 'delete':
             # Delete file.
-            response = service.files().delete(
-                fileId=item.get('id'),
-            ).execute()
+            try:
+                response = service.files().delete(
+                    fileId=item.get('id'),
+                ).execute()
+            except Exception as e:
+                print(f"Error: {e}")
             if not response:
                 result = True
         elif type == 'trash':
             # Move file to the trash.
             body = {"trashed": True}
-            response = service.files().update(
-                fileId=item.get('id'),
-                body=body,
-            ).execute()
+            try:
+                response = service.files().update(
+                    fileId=item.get('id'),
+                    body=body,
+                ).execute()
+            except Exception as e:
+                print(f"Error: {e}")
             if response.get('id'):
                 result = True
     except Exception as e:
@@ -43,33 +49,7 @@ def remove_drive_item(service, item, type='delete'):
         exit(1)
     return result
 
-def get_shared_drive_list(service, name, page_token=None):
-    """Get results of search query on specified account."""
-    all_results = []
-    while True:
-        try:
-            response = service.drives().list(pageToken=page_token).execute()
-        except Exception as e:
-            print(f"Error: {e}")
-            exit(1)
-        for item in response.get('drives', []):
-            all_results.append(item)
-        page_token = response.get('nextPageToken', None)
-        if page_token is None:
-            break
-    return all_results
-
-def get_shared_drive(service, name_string=''):
-    """Search for "folder_string" among Drive folders and folder IDs."""
-    all_results = get_shared_drive_list(service, name_string)
-    results = []
-    for r in all_results:
-        if r['name'] == name_string:
-            results.append(r)
-    item = dutils.choose_item(service, results)
-    return item
-
-def create_folder_in_shared_drive(user, service, item, metadata):
+def create_folder_in_shared_drive(service, item, metadata):
     # Re-create the folder under the destination.
     #   Returns new-item id dict.
     try:
@@ -83,7 +63,7 @@ def create_folder_in_shared_drive(user, service, item, metadata):
         exit(1)
     return result
 
-def move_item_to_shared_drive(user, service, item, new_parents):
+def move_item_to_shared_drive(service, item, new_parents):
     # Move the file into the destination.
     try:
         result = service.files().update(
@@ -97,20 +77,25 @@ def move_item_to_shared_drive(user, service, item, new_parents):
         exit(1)
     return result
 
-def move_items_recursively(user, service, item, destination, dest_drive):
+def move_items_recursively(service, item, destination, dest_drive):
     if not dutils.item_is_folder(item):
         # Move file.
         new_parent = destination.get('id')
-        new_id = move_item_to_shared_drive(user, service, item, new_parent)
-        if new_id:
-            new_item = dutils.get_drive_item(service, new_id.get('id'))
+        new_item_id = move_item_to_shared_drive(service, item, new_parent)
+        if new_item_id:
+            new_item = dutils.get_drive_item(service, new_item_id.get('id'))
         # Can't just use the returned item from "move" b/c it's lacking parent info.
-        # new_item = move_item_to_shared_drive(user, service, item, new_parent)
         show_result(service, new_item, item)
     else:
         # Move folder and children.
         #   Check to see if folder exists in destination.
-        new_parent = dutils.find_drive_item(service, name_string=item.get('name'), shared_drive=dest_drive)
+        #       List destination's children by name.
+        dest_children = dutils.get_children(service, destination.get('id'), shared_drive=dest_drive)
+        new_parent = None
+        for dest_child in dest_children:
+            if dest_child.get('name') == item.get('name'):
+                new_parent = dest_child
+                break
         if not new_parent:
             # Set the new folder's metadata.
             metadata = {
@@ -119,8 +104,7 @@ def move_items_recursively(user, service, item, destination, dest_drive):
                 'mimeType': 'application/vnd.google-apps.folder',
             }
             # Re-create the folder under the destination.
-            new_id = create_folder_in_shared_drive(user, service, item, metadata)
-            new_parent = None
+            new_id = create_folder_in_shared_drive(service, item, metadata)
             if new_id:
                 new_parent = dutils.get_drive_item(service, new_id.get('id'))
             show_result(service, new_parent, item)
@@ -128,7 +112,7 @@ def move_items_recursively(user, service, item, destination, dest_drive):
         # Move children to new folder.
         children = dutils.get_children(service, item['id'])
         for child in children:
-            move_items_recursively(user, service, child, new_parent, dest_drive)
+            move_items_recursively(service, child, new_parent, dest_drive)
         # Remove empty folder.
         remove_drive_item(service, item)
 
@@ -136,9 +120,8 @@ def run_move_folder(user, service, folder, destination_string):
     # Ensure valid destination drive and folder.
     path_parts = destination_string.split('>')
     dest_drive_string = destination_string.split('>')[0].strip()
-    dest_drive = get_shared_drive(service, name_string=dest_drive_string)
+    dest_drive = dutils.get_shared_drive(service, name_string=dest_drive_string)
     parent_folder_string = destination_string.split('>')[-1].strip()
-
 
     ready = False
     error = f"Error: Shared Drive \"{dest_drive_string}\" not found."
@@ -160,5 +143,5 @@ def run_move_folder(user, service, folder, destination_string):
         print(error)
         return 1
 
-    print(f"Moving \"{folder['name']}\" recursively to \"{parent_folder['name']}\" ...")
-    move_items_recursively(user, service, folder, parent_folder, dest_drive)
+    print(f"Moving \"{folder['name']}\" recursively to \"{parent_folder.get('name')}\" ({parent_folder.get('id')})...")
+    move_items_recursively(service, folder, parent_folder, dest_drive)
